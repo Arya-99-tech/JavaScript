13dceef2a781937375cf526ddd4f16a19007cf25
Arya K Rajeev <aryakrajeev85@gmail.com>
2025-08-21 18:57:16 +0000

    Array completed

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -20,14 +20,40 @@ const newArr=myArr.join()
 
 // slice(start,end)  , 
 
-console.log("A ",myArr); //A  [ 0, 1, 2, 3, 4, 5 ]
-const myn1=myArr.slice(1,3)
-console.log(myn1);//[ 1, 2 ]
-console.log("B ",myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
+// console.log("A ",myArr); //A  [ 0, 1, 2, 3, 4, 5 ]
+// const myn1=myArr.slice(1,3)
+// console.log(myn1);//[ 1, 2 ]
+// console.log("B ",myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
 
 //splice(start,deleteCount,..item)
-const myn2=myArr.splice(1,3) 
-console.log(myn2);//[ 1, 2, 3 ]
-console.log("c ",myArr);//c  [ 0, 4, 5 ]
+// const myn2=myArr.splice(1,3) 
+// console.log(myn2);//[ 1, 2, 3 ]
+// console.log("c ",myArr);//c  [ 0, 4, 5 ]
+
+
+const marvel_heros=["thor","Ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros); //[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]); //flash
+
+const concated =marvel_heros.concat(dc_heros)
+//console.log(concated); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const all_new_heros=[...marvel_heros,...dc_heros]
+// console.log(all_new_heros); //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+//console.log(real_another_array);//[1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
+
+// console.log(Array.isArray("Arunima"));//false
+// console.log(Array.from("Arunima")); //['A', 'r', 'u','n', 'i', 'm','a']
+//console.log(Array.from({name: "arunima"})); 
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));//[ 100, 200, 300 ]
 
 
