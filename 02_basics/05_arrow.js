86152881edde9f590e05116d441e064b87566a7e
Arya K Rajeev <aryakrajeev85@gmail.com>
2025-11-07 13:44:28 +0000

    Add object basics example

---
+++ 02_basics/05_arrow.js
@@ -0,0 +1,11 @@
+  // arrow function syntax ()=>{}
+    //  const addtwo= (num1,num2)=>{  this is explicit return ,when we use explicitly return keyword
+    //     return num1+num2   if u are using curly brcaes then return keyword  needed 
+    //  }
+    //  console.log(addtwo(8,9));
+     
+
+    //  const addtwo= (num1,num2)=> num1+num2; // implicit return is useful for one liner code 
+     //const addtwo= (num1,num2)=> (num1+num2);  //   if you are using parenthesis then no need of return keyword 
+     //const addtwo= (num1,num2)=>({name:'arya'}) // if you want to return an object u need to use parenthesis other wise it will give undefined 
+     //console.log(addtwo(8,9));
